5c157bd457edb983baae383ccda16488db4fc9c5
pabs <pablosgrad12@gmail.com>
2023-09-24 09:57:35 -0600

    Attempted solution

--- code.js
+++ code.js
@@ -1,3 +1,21 @@
 function divideAndConquerSum(a) {
-    return a;
+    return helper(a, 0, a.length - 1);
+}
+
+function helper(arr, lo, hi) {
+    //Base cases
+    if (lo > hi) { return 0; }
+    if (lo === hi) {
+        return arr[lo];
+    }
+    //If there are only two elements
+    if (hi - lo + 1 === 2) {
+        return arr[lo] + arr[hi];
+    }
+
+
+    let third = lo + Math.floor((hi - lo + 1) / 3);
+    let third2 = lo + 2 * Math.floor((hi - lo + 1) / 3);
+
+    return helper(arr, lo, third - 1) + helper(arr, third, third2 - 1) + helper(arr, third2, hi);
 }
